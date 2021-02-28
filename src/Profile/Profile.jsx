efd14b8fb074c673220a787695448e72332027be
Alexandra Zhdanova <alexandra.zhdanova@apliteni.com>
2021-02-28 11:56:05 +0300

    DEV-2 refactoring Profile

--- src/Profile/Profile.jsx
+++ src/Profile/Profile.jsx
@@ -1,23 +1,115 @@
 import React from 'react'
+import {Box, Button, Paper, TextField, Typography} from "@material-ui/core";
+import {LogoForCard} from "./LogoForCard/LogoForCard";
+import {LogoChipCode} from "./LogoChipCode/LogoChipCode";
 
 export const Profile = ({navigateTo}) => {
   return (
-    <>
-      <h1>Профиль</h1>
-      <p>Введите платежные данные</p>
-      <form>
-        <input id='name' type='text' name='name' size='58' placeholder='Имя владельца*'/>
-        <br/>
-        <br/>
-        <input id='numberCard' type='text' name='numberCard' size='58' placeholder='Номер карты*'/>
-        <br/>
-        <br/>
-        <input id='date' type='text' name='date' size='28' placeholder='MM/YY*'/>
-        <input id='cvc' type='text' name='cvc' size='28' placeholder='CVC*'/>
-        <br/>
-        <br/>
-        <button onClick={()=> navigateTo('maps')}>Сохранить</button>
-      </form>
-    </>
+    <Box display='flex' justifyContent='center' alignItems="center" paddingTop='10rem'>
+      <Paper elevation={2} variant="outlined" square={false} style={{
+        borderRadius: '10px', padding: "60px 44px"
+      }}
+      >
+        <Box
+          display='flex'
+          justifyContent='center'
+          alignItems='center'
+          flexDirection='column'>
+          <Typography
+            variant="h4">Профиль</Typography>
+          <Typography variant="body1">Введите платежные данные</Typography>
+          <Box padding='40px 0'>
+            <form>
+              <Box display='flex' justifyItems='row' padding="40px 0">
+                <Box marginRight='100px'>
+                  <TextField
+                    id="name"
+                    label='Имя владельца'
+                    type="text"
+                    name="name"
+                    margin="normal"
+                    fullWidth
+                    color="primary"
+                    required
+                  />
+
+                  <TextField
+                    id="numberCard"
+                    label='Номер карты'
+                    type="text"
+                    name="numberCard"
+                    margin="normal"
+                    fullWidth
+                    color="primary"
+                    required
+                  />
+                  <Box display='flex'>
+                    <TextField
+                      id="date"
+                      label='MM/YY'
+                      type="date"
+                      name="date"
+                      margin="normal"
+                      fullWidth
+                      color="primary"
+                      required
+                      style={{paddingRight: "35px"}}
+                    />
+                    <TextField
+                      id="cvc"
+                      label='CVC'
+                      type="text"
+                      name="cvc"
+                      margin="normal"
+                      fullWidth
+                      color="primary"
+                      required
+                    />
+                  </Box>
+                </Box>
+                <Paper elevation={5} variant="outlined" square={false}
+                       style={{
+                         borderRadius: '10px', width: "347px",
+                         height: "215px",
+                         padding: "18px 28px",
+                         boxSizing: "border-box",
+                         display: 'flex',
+                         flexDirection: "column",
+                         justifyContent: 'space-between'
+                       }}>
+                  <Box display='flex' justifyItems='row' justifyContent='space-between'>
+                    <LogoForCard/>
+                    <Typography variant='body1'>MM/YY</Typography>
+                  </Box>
+                  <Typography variant='body1' style={{
+                    fontSize: "22px",
+                    lineHeight: "1.2"
+                  }}>0000 0000 0000 0000</Typography>
+                  <Box display='flex' justifyItems='row' justifyContent='space-between'>
+                    <LogoChipCode/>
+                    <img width="45px"
+                         src='https://raw.githubusercontent.com/satansdeer/loft-taxi-mui-theme/3f2f1ed65d5c650afc969e0f4b03516969aa44a6/src/MCIcon/mc_symbol.svg'
+                         alt="mastercard"/>
+                  </Box>
+                </Paper>
+              </Box>
+              <div style={{
+                display: "flex",
+                justifyContent: "center"
+              }}>
+                <Button type='submit' variant='contained' color='primary'
+                        style={{
+                          borderRadius: "40px",
+                          fontSize: "1.3rem",
+                          width: "350px",
+                        }}
+                        onClick={() => navigateTo('maps')}>Сохранить</Button>
+              </div>
+
+            </form>
+          </Box>
+        </Box>
+      </Paper>
+    </Box>
   )
 }
