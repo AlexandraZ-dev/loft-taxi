099e059bef7919c6ce6e83cc380fc3305dfdd4aa
Alexandra Zhdanova <alexandra.zhdanova@apliteni.com>
2021-02-28 11:54:44 +0300

    DEV-2 refactoring Header

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,30 +1,51 @@
 import React from 'react';
-import './Header.css';
+import {AppBar, Button, Container, Grid, makeStyles, Toolbar} from "@material-ui/core";
+import {Logo} from "./Logo/Logo";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  header: {
+    backgroundColor: "black",
+    color: "white"
+  },
+  button: {
+    "&.active": {
+      background:'black',
+    },
+  },
+}));
 
 export const Header = ({navigateTo}) => {
-
+  const classes = useStyles();
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <button onClick={() => navigateTo('auth')
-            }>Регистрация
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigateTo('maps')
-            }>Maps
-            </button>
-          </li>
-          <li>
-            <button onClick={() => navigateTo('profile')
+    <AppBar position="static" className={classes.header}>
+      <Container>
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="center"
+        >
+          <Logo/>
+          <Toolbar disableGutters={true}>
+            <Button color="inherit" onClick={() => navigateTo('maps')
+            }>Карты
+            </Button>
+
+            <Button color="inherit" onClick={() => navigateTo('profile')
             }>Profile
-            </button>
-          </li>
-        </ul>
-      </nav>
-    </header>
+            </Button>
+
+            <Button color="inherit" onClick={() => navigateTo('login')
+            }>Выйти
+            </Button>
+          </Toolbar>
+
+        </Grid>
+      </Container>
+    </AppBar>
   )
 
 }
